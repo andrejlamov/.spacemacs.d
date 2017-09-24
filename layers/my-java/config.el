9d10504558c9e1ac6969fb3be998a9e72754bddc
Andrej Lamov <andrej.lamov@gmail.com>
2017-09-24 22:13:57 +0200

    java: Use use-package hooks instead of advice for adding keys

--- layers/my-java/config.el
+++ layers/my-java/config.el
@@ -1,1 +1,5 @@
-(advice-add 'java/init-eclim :after #'my-java/add-keys)
+(spacemacs|use-package-add-hook eclim
+  :post-config
+  (progn
+    (spacemacs/set-leader-keys-for-major-mode 'java-mode
+      "mc" 'my-java/maven-test-class)))
--- layers/my-java/funcs.el
+++ layers/my-java/funcs.el
@@ -1,7 +1,3 @@
 (defun my-java/maven-test-class ()
   (interactive)
   (eclim-maven-run (concat "-q clean test -DtrimStackTrace=false -Dtest=" (file-name-base) "*")))
-
-(defun my-java/add-keys ()
-    (spacemacs/set-leader-keys-for-major-mode 'java-mode
-      "mc" 'my-java/maven-test-class))
