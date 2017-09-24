5dc0f0bfe1d845da2a1e376c2b8ec84f9405fdfc
Andrej Lamov <andrej.lamov@gmail.com>
2017-09-24 22:04:54 +0200

    clojure: Send optional expression to cljs repl on "refres & test"

--- layers/my-clojure/config.el
+++ layers/my-clojure/config.el
@@ -1,2 +1,9 @@
-(advice-add 'cider-refresh--handle-response :after #'my-clojure/test-or-show-refresh-log)
-(advice-add 'cider-refresh :before #'my-clojure/save-if-file-buffer)
+(spacemacs|use-package-add-hook cider
+  :post-config
+  (progn
+    (dolist (m '(clojure-mode clojurescript-mode))
+      (spacemacs/set-leader-keys-for-major-mode m
+        "e!" 'my-clojure/set-cljs-expr))
+
+    (advice-add 'cider-refresh--handle-response :after #'my-clojure/test-or-show-refresh-log)
+    (advice-add 'cider-refresh :before #'my-clojure/save-if-file-buffer)))
--- layers/my-clojure/funcs.el
+++ layers/my-clojure/funcs.el
@@ -1,12 +1,28 @@
+(defvar my-clojure/cljs-expr "")
+
 (defun my-clojure/test-or-show-refresh-log (response _log-buffer)
   (let ((status (nrepl-dbind-response response
                     (out err reloading status error error-ns after before)
                   status)))
     (pcase status
-      ('("ok")    (cider-test-run-project-tests))
+      ('("ok")    (progn
+                      (cider-test-run-project-tests)
+                      (when (not (string= "" my-clojure/cljs-expr))
+                        (my-clojure/eval-in-cljs-repl my-clojure/cljs-expr))))
       ('("error") (popwin:popup-buffer "*cider-refresh-log*"))
       (other nil))))
 
 (defun my-clojure/save-if-file-buffer (&optional args)
   (when (buffer-file-name)
     (save-buffer)))
+
+(defun my-clojure/set-cljs-expr (expr)
+  (interactive "Mcljs expr: ")
+  (setq my-clojure/cljs-expr expr)
+  (my-clojure/eval-in-cljs-repl expr))
+
+(defun my-clojure/eval-in-cljs-repl (expr)
+  (with-current-buffer (cider-current-repl-buffer "cljs")
+    (end-of-buffer)
+    (insert expr)
+    (cider-repl-return)))
