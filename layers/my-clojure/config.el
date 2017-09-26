344ba85ee6dabbd5c61c4c772fd0f59fcbe7efb2
Andrej Lamov <andrej.lamov@gmail.com>
2017-09-26 23:13:05 +0200

    clojure: Use "e!" in clojurec-mode as well

--- layers/my-clojure/config.el
+++ layers/my-clojure/config.el
@@ -1,7 +1,7 @@
 (spacemacs|use-package-add-hook cider
   :post-config
   (progn
-    (dolist (m '(clojure-mode clojurescript-mode))
+    (dolist (m '(clojurec-mode clojure-mode clojurescript-mode))
       (spacemacs/set-leader-keys-for-major-mode m
         "e!" 'my-clojure/set-cljs-expr))
 
